1b54e2e3e9013c221ce4fe666541f06e5cdf6221
Nicole Anne Lopez <lopez.nicoleanne@gmail.com>
2020-03-28 22:53:37 +0800

    Revised CSS for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,13 @@ const Home = () => (
     <NewLayout>
         <div className="banner-content">
             <img src={nixlogo} />
-            <h1 className="mb-20">NICOLE <span className="pink">LOPEZ</span></h1>
-            <p>
-                <span>I am a </span>
-                <span className="cd-words-wrapper pink">Full-stack Web Developer</span>
-            </p>
+            <div className="banner-content__text-wrapper">
+                <h1 className="mb-20">NICOLE <span className="pink">LOPEZ</span></h1>
+                <p>
+                    <span>I am a </span>
+                    <span className="cd-words-wrapper pink">Full-stack Web Developer</span>
+                </p>
+            </div>
         </div>
     </NewLayout>
     
