c67eef2fcc67f9336ec85a0cb4ce9024c367191c
Nicole Anne Lopez <lopez.nicoleanne@gmail.com>
2020-04-05 12:31:38 +0800

    Added social buttons and testimonials

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,13 @@
-import PropTypes from "prop-types"
-import React from "react"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import PropTypes from 'prop-types'
+import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 
-import "./sidebar.css"
+import './sidebar.css'
 
 import devpic from '../images/devpic.jpg'
 
 const POST_PAGES_QUERY = graphql`
-  query getMainPages{
+  query getMainPages {
     site {
       siteMetadata {
         title
@@ -22,40 +22,73 @@ const POST_PAGES_QUERY = graphql`
   }
 `
 
-const Sidebar = ({ siteTitle}) => {
+const Sidebar = ({ siteTitle }) => {
   const data = useStaticQuery(POST_PAGES_QUERY)
 
-    return (
-      <>
+  return (
+    <>
       <div id="sidebar-container" className="sidebar-container">
         <div className="sidebar">
           <div>
             <img src={devpic}></img>
-            <div className='sidebar__site-name'>{ siteTitle }</div>
+            <div className="sidebar__site-name">{siteTitle}</div>
           </div>
           <div>
             <ul className="sidebar__main-link-group">
-                {data.site.siteMetadata.mainPages.map(page => (
-                    <li key={page.link}>
-                        <Link className="sidebar__main-link" to={`${page.link}`}>
-                          <span className="sidebar__main-link-icon">
-                            <i className={`lnr ${page.icon}`}></i>
-                          </span>
-                          {page.name}
-                        </Link>
-                    </li>
-                ))}
+              {data.site.siteMetadata.mainPages.map(page => (
+                <li key={page.link}>
+                  <Link className="sidebar__main-link" to={`${page.link}`}>
+                    <span className="sidebar__main-link-icon">
+                      <i className={`lnr ${page.icon}`}></i>
+                    </span>
+                    {page.name}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
 
           <footer>
-              © {new Date().getFullYear()}
+            <div className="social">
+              <ul className="social-buttons">
+                <li className="social-buttons__item">
+                  <a
+                    href="https://www.facebook.com/nicanixnicanix"
+                    target="_blank"
+                  >
+                    <i className="social fab fa-facebook-square"></i>
+                  </a>
+                </li>
+                <li className="social-buttons__item">
+                  <a
+                    href="https://www.twitter.com/NicaNixNicaNix"
+                    target="_blank"
+                  >
+                    <i className="social fab fa-twitter-square"></i>
+                  </a>
+                </li>
+                <li className="social-buttons__item">
+                  <a
+                    href="https://www.linkedin.com/in/nicoleannelopez"
+                    target="_blank"
+                  >
+                    <i className="social fab fa-linkedin"></i>
+                  </a>
+                </li>
+                <li className="social-buttons__item">
+                  <a href="https://github.com/nalopez" target="_blank">
+                    <i className="social fab fa-github-square"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            © {new Date().getFullYear()}
           </footer>
         </div>
       </div>
     </>
-    )
-            }
+  )
+}
 
 Sidebar.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,142 +1,178 @@
-import React from "react"
+import React from 'react'
 
 // Components
-import NewLayout from "../components/newlayout"
-import PageHeading from "../components/pageheading"
+import NewLayout from '../components/newlayout'
+import PageHeading from '../components/pageheading'
 
 // CSS
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "../components/aboutme.css"
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import '../components/aboutme.css'
 
 // Images
-import baxLogo from "../images/clients/bax-logo.svg"
-import codingchiefsLogo from "../images/clients/cc-logo.png"
-import insurancehubLogo from "../images/clients/tih-logo.png"
-import EvoraAngelo from "../images/testimonials/evora-angelo.jpg"
+import baxLogo from '../images/clients/bax-logo.svg'
+import codingchiefsLogo from '../images/clients/cc-logo.png'
+import insurancehubLogo from '../images/clients/tih-logo.png'
+import EvoraAngelo from '../images/testimonials/evora-angelo.jpg'
+import VillasZedrick from '../images/testimonials/villas-zedrick.jpeg'
 
 const AboutMe = () => (
-    // TODO: Fix passing of background
-    // TODO: Download resume function
-    // TODO: Create sprites for clients and testimonials
-    <NewLayout background="#2C2D2F">
-        <PageHeading headingStr="About Me." classStr="lnr lnr-user"></PageHeading>
-        <div className="about-me mb-70">
-            <div className="row mb-70">
-                <div className="about-me__main-message">
-                    <h3 className="mb-20">Full Stack Developer & Scrum Master</h3>
-                    <p>I am a fullstack PHP developer for 5 years, specializing in the development of payment management and e-commerce sites.</p>
+  // TODO: Download resume function
+  // TODO: Create sprites for clients and testimonials
+  <NewLayout background="#2C2D2F">
+    <PageHeading headingStr="About Me." classStr="lnr lnr-user"></PageHeading>
+    <div className="about-me mb-70">
+      <div className="row mb-70">
+        <div className="about-me__main-message">
+          <h3 className="mb-20">Full Stack Developer & Scrum Master</h3>
+          <p>
+            I am a fullstack PHP developer for 5 years, specializing in the
+            development of payment management and e-commerce sites.
+          </p>
 
-                    <p>I am a certified Scrum Master, with 2 years experience of facilitiating scrum activities.</p>
+          <p>
+            I am a certified Scrum Master, with 2 years experience of
+            facilitiating scrum activities.
+          </p>
 
-                    <p>I am a freelance UI/UX designer.</p>
-                </div>
-
-                <div className="about-me__personal-info">
-                    <h3 className="mb-20">Personal Information</h3>
-                    <ul className="about-me__personal-info-list">
-                        <li>
-                            <span className="about-me__personal-info-label">Name</span>
-                            <span>Nicole Anne Lopez</span>
-                        </li>
-                        <li>
-                            <span className="about-me__personal-info-label">Age</span>
-                            <span>25 Years</span>
-                        </li>
-                        <li>
-                            <span className="about-me__personal-info-label">Residence</span>
-                            <span>Clark, Pampanga</span>
-                        </li>
-                        <li>
-                            <span className="about-me__personal-info-label">Name</span>
-                            <span>lopez.nicoleanne@gmail.com</span>
-                        </li>
-                        <li>
-                            <span className="about-me__personal-info-label">Phone Number:</span>
-                            <span>(+63) 961-361-3660</span>
-                        </li>
-                        <li>
-                            <span className="about-me__personal-info-label">Freelance:</span>
-                            <span>Available</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            
-            <div className="row about-me__services mb-50">
-                <div className="subheading">
-                    <h3> Services </h3>
-                </div>
-                <div className="services__list">
-                    <div className="services__item">
-                        <div className="services__icon">
-                            <i className="lnr lnr-laptop"></i>
-                        </div>
-                        <h4>Web Development</h4>
-                        <p>Web infrastructure planning, development, deployment, maintenance and support</p>
-                    </div>
-                    <div className="services__item">
-                        <div className="services__icon">
-                            <i className="lnr lnr-laptop"></i>
-                        </div>
-                        <h4>Agile Management</h4>
-                        <p>Scrum team facilitation (e.g. Sprint Planning, Sprint Restrospective)</p>
-                    </div>
-                    <div className="services__item">
-                        <div className="services__icon">
-                            <i className="lnr lnr-laptop"></i>
-                        </div>
-                        <h4>Freelance Projects</h4>
-                        <p>Logo design</p>
-                    </div>
-                </div>
-            </div>
-            <div className="row about-me__clients mb-50">
-                <div className="subheading">
-                    <h3> Clients </h3>
-                </div>
-                <div className="clients__list">
-                    <div className="clients__logo">
-                        <a href="https://www.bax-shop.nl">
-                            <img src={baxLogo} />
-                        </a>
-                    </div>
-                    <div className="clients__logo">
-                        <a href="https://www.codingchiefs.com/en">
-                            <img src={codingchiefsLogo} />
-                        </a>
-                    </div>
-                    <div className="clients__logo">
-                        <a href="https://www.facebook.com">
-                            <img src={insurancehubLogo} />
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className="row about-me__testimonials mb-50">
-                <div className="subheading">
-                    <h3> Testimonials </h3>
-                </div>
-
-                <div className="testimonials__list">
-
-                    <div className="testimonial-item">
-                        <div className="testimonial-item__content">
-                            <p>"Nix is easy to work with because of her wide experience in programming. She is professional and does her work on time."</p>
-                        </div>
-                        <div className="testimonial-item__meta">
-                            <img src={ EvoraAngelo } />
-                            <div className="testimonial-item__meta-info">
-                                <h4>Angelo Evora</h4>
-                                <div>Owner, The Insurance Hub <br/> Financial Adviser, Pru Life UK</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+          <p>I am a freelance UI/UX designer.</p>
         </div>
-    </NewLayout>
+
+        <div className="about-me__personal-info">
+          <h3 className="mb-20">Personal Information</h3>
+          <ul className="about-me__personal-info-list">
+            <li>
+              <span className="about-me__personal-info-label">Name</span>
+              <span>Nicole Anne Lopez</span>
+            </li>
+            <li>
+              <span className="about-me__personal-info-label">Age</span>
+              <span>25 Years</span>
+            </li>
+            <li>
+              <span className="about-me__personal-info-label">Residence</span>
+              <span>Clark, Pampanga</span>
+            </li>
+            <li>
+              <span className="about-me__personal-info-label">Name</span>
+              <span>lopez.nicoleanne@gmail.com</span>
+            </li>
+            <li>
+              <span className="about-me__personal-info-label">
+                Phone Number:
+              </span>
+              <span>(+63) 961-361-3660</span>
+            </li>
+            <li>
+              <span className="about-me__personal-info-label">Freelance:</span>
+              <span>Available</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="row about-me__services mb-50">
+        <div className="subheading">
+          <h3> Services </h3>
+        </div>
+        <div className="services__list">
+          <div className="services__item">
+            <div className="services__icon">
+              <i className="lnr lnr-laptop"></i>
+            </div>
+            <h4>Web Development</h4>
+            <p>
+              Web infrastructure planning, development, deployment, maintenance
+              and support
+            </p>
+          </div>
+          <div className="services__item">
+            <div className="services__icon">
+              <i className="lnr lnr-chart-bars"></i>
+            </div>
+            <h4>Agile Management</h4>
+            <p>
+              Scrum team facilitation (e.g. Sprint Planning, Sprint
+              Restrospective)
+            </p>
+          </div>
+          <div className="services__item">
+            <div className="services__icon">
+              <i className="lnr lnr-eye"></i>
+            </div>
+            <h4>Freelance Projects</h4>
+            <p>Logo design</p>
+          </div>
+        </div>
+      </div>
+      <div className="row about-me__clients mb-50">
+        <div className="subheading">
+          <h3> Clients </h3>
+        </div>
+        <div className="clients__list">
+          <div className="clients__logo">
+            <a href="https://www.bax-shop.nl">
+              <img src={baxLogo} />
+            </a>
+          </div>
+          <div className="clients__logo">
+            <a href="https://www.codingchiefs.com/en">
+              <img src={codingchiefsLogo} />
+            </a>
+          </div>
+          <div className="clients__logo">
+            <a href="https://www.facebook.com">
+              <img src={insurancehubLogo} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="row about-me__testimonials mb-50">
+        <div className="subheading">
+          <h3> Testimonials </h3>
+        </div>
+
+        <div className="testimonials__list">
+          <div className="testimonial-item">
+            <div className="testimonial-item__content">
+              <p>
+                "Nix is easy to work with because of her wide experience in
+                programming. She is professional and does her work on time."
+              </p>
+            </div>
+            <div className="testimonial-item__meta">
+              <img src={EvoraAngelo} />
+              <div className="testimonial-item__meta-info">
+                <h4>Angelo Evora</h4>
+                <div>
+                  Owner, The Insurance Hub <br /> Financial Adviser, Pru Life UK
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="testimonial-item">
+            <div className="testimonial-item__content">
+              <p>
+                "Nicole has been a real pleasure to work with. She did an
+                exceptional job on projects that we worked on together.
+                Passionate, great, and creative co-worker. Detail-oriented,
+                intelligent, deadline-oriented, and broad-minded colleague. She
+                has a vast knowledge and is thorough with her work."
+              </p>
+            </div>
+            <div className="testimonial-item__meta">
+              <img src={VillasZedrick} />
+              <div className="testimonial-item__meta-info">
+                <h4>Zedrick John Villas</h4>
+                <div>Program Scrum Master, AXA Philippines</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </NewLayout>
 )
 
 export default AboutMe
